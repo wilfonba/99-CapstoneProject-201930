f06b5b7c4f19bcd22b31355be2af7c247093503b
sorensms <sorensms@rose-hulman.edu>
2019-05-16 11:49:25 -0400

    added name

--- src/m1_laptop_code.py
+++ src/m1_laptop_code.py
@@ -20,14 +20,77 @@ def get_my_frame(root, window, mqtt_sender):
     # Construct your frame:
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
     frame_label = ttk.Label(frame, text="Maddie Sorensen")
-    frame_label.grid()
+    frame_label.grid(row=0,column=2)
     # Done: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
     # TODO: Put your GUI onto your frame (using sub-frames if you wish).
 
+
+
+
+    entry_distance=ttk.Entry(frame, width=8)
+    entry_distance.insert(0,"100")
+    entry_distance_label= ttk.Label(frame, text="Distance")
+    entry_distance_label.grid(row=2, column=2)
+    entry_distance.grid(row=3, column=2)
+
+    entry_speed = ttk.Entry(frame, width=8)
+    entry_speed.insert(0, "100")
+    entry_speed.grid(row=5, column=2)
+    entry_speed_label = ttk.Label(frame, text="Speed (0 to 100)")
+    entry_speed_label.grid(row=4, column=2)
+
+    forward_button = ttk.Button(frame, text="Move Forward")
+    forward_button.grid(row=1, column=0)
+    forward_button['command'] = lambda: handle_move_forward(entry_speed, entry_distance, mqtt_sender)
+
+    backward_button = ttk.Button(frame, text="Move Backward")
+    backward_button.grid(row=1, column=3)
+    backward_button['command'] = lambda: handle_move_backward(entry_speed, entry_distance, mqtt_sender)
+
+    go_until_button = ttk.Button(frame, text="Go Until")
+    go_until_button.grid(row=1, column=4)
+    go_until_button['command'] = lambda: handle_go_until(X_entry, delta_entry, entry_speed, mqtt_sender)
+
+    X_entry = ttk.Entry(frame, width=8)
+    X_entry.insert(0, "100")
+    X_entry.grid(row=3, column=4)
+    X_label = ttk.Label(frame, text="Distance from Object")
+    X_label.grid(row=2, column=4)
+
+    delta_entry = ttk.Entry(frame, width=8)
+    delta_entry.insert(0, "100")
+    delta_entry.grid(row=5, column=4)
+    delta_label = ttk.Label(frame, text="Delta")
+    delta_label.grid(row=4, column=4)
+
+
+
+
     # Return your frame:
     return frame
+
+def handle_move_forward(entry_speed, entry_distance,mqtt_sender):
+    print("move_forward:", entry_distance.get())
+    speed=int(entry_speed.get())
+    distance=int(entry_distance.get())
+    mqtt_sender.send_message("move_forward",[speed, distance])
+
+def handle_move_backward(entry_speed, entry_distance,mqtt_sender):
+    print("move_backward:", entry_distance.get())
+    speed=(-int(entry_speed.get()))
+    distance=int(entry_distance.get())
+    mqtt_sender.send_message("move_backward",[speed, distance])
+
+
+def handle_go_until(entry_speed, X_entry, delta_entry, mqtt_sender):
+    speed=int(entry_speed.get())
+    X=int(X_entry.get())
+    delta=int(delta_entry.get())
+    mqtt_sender.send_message("go_until",[speed,X,delta])
+
+
 
 
 class MyLaptopDelegate(object):
--- src/m1_robot_code.py
+++ src/m1_robot_code.py
@@ -1,10 +1,10 @@
 """
   Capstone Project.  Code to run on the EV3 robot (NOT on a laptop).
   Author:  Your professors (for the framework)
-    and PUT_YOUR_NAME_HERE.
+    and Maddie Sorensen.
   Spring term, 2018-2019.
 """
-# TODO 1:  Put your name in the above.
+# Done:  Put your name in the above.
 
 import mqtt_remote_method_calls as mqtt
 import rosebot
@@ -18,10 +18,12 @@ class MyRobotDelegate(object):
     gets a message (name of the method, plus its arguments)
     from a LAPTOP via MQTT.
     """
+
     def __init__(self, robot):
         self.robot = robot  # type: rosebot.RoseBot
         self.mqtt_sender = None  # type: mqtt.MqttClient
         self.is_time_to_quit = False  # Set this to True to exit the robot code
+        self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
 
     def set_mqtt_sender(self, mqtt_sender):
         self.mqtt_sender = mqtt_sender
@@ -31,7 +33,71 @@ class MyRobotDelegate(object):
         print_message_received("go", [left_motor_speed, right_motor_speed])
         self.robot.drive_system.go(left_motor_speed, right_motor_speed)
 
-    # TODO: Add methods here as needed.
+    # Done: Add methods here as needed.
+    def move_forward(self, speed, distance):
+        print('speed', speed, distance)
+        self.robot.drive_system.go(speed, speed)
+        self.robot.drive_system.left_motor.reset_position()
+        while True:
+            if (self.robot.drive_system.left_motor.get_position()/85)>=distance:
+                self.robot.drive_system.stop()
+                break
+
+    def move_backward(self, speed, distance):
+        print('speed backward', speed, distance)
+        self.robot.drive_system.go(speed,speed)
+        self.robot.drive_system.left_motor.reset_position()
+        while True:
+            if abs((self.robot.drive_system.left_motor.get_position()/85))>=distance:
+                self.robot.drive_system.stop()
+                break
+
+    def go_until(self,speed,X,delta):
+        self.robot.drive_system.left_motor.reset_position()
+        original=self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()-X
+        go_straight=original*(1/2)
+        self.robot.drive_system.go(speed, speed)
+        print("go:1")
+        while True:
+            print("true")
+            if abs((self.robot.drive_system.left_motor.get_position()/85))>=go_straight:
+                break
+
+        new_speed = (1 / 2) * speed
+        self.robot.drive_system.go(new_speed, new_speed)
+        print("go:2")
+        while True:
+            a = self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            b = self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            c = self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            d = self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            e = self.robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            if a<10000:
+                small=a
+            if a>0:
+                big=a
+            if b<small:
+                small=b
+            if b>big:
+                big=b
+            if c<small:
+                small=c
+            if c>big:
+                big=c
+            if d<small:
+                small=d
+            if d>big:
+                big=d
+            if e<small:
+                small=e
+            if e>big:
+                big=e
+            average=(a+b+c+d+e-small-big)/3
+            if average<=(X+delta) and average>=(X-delta):
+
+                break
+        self.robot.drive_system.stop()
+
 
 
 def print_message_received(method_name, arguments):
